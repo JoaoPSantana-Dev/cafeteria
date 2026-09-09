d908fec7d5021b1d32c974a9c64eb7895c78d14b
joao@gmail.com <joao@gmail.com>
2026-09-09 14:31:53 -0300

    Tela inicial

--- App.tsx
+++ App.tsx
@@ -1,39 +1,93 @@
 import { Ionicons } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { ScrollView } from 'react-native/types_generated/index';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={style.container}>
-      <View style={style.header}>
-        <Text>Café do Código</Text>
-        <Text>Seu café, uma linha por vez</Text>
+    <ScrollView style={styles.container}>
+
+      {/* Header */}
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.headerTitle}>Café do Código</Text>
+          <Text style={styles.headerSubtitle}>Seu café, uma linha por vez</Text>
+        </View>
+
+        <View style={styles.avatarPlaceHolder} >
+          <Ionicons name="person" size={20} color="rgb(20, 14, 11)"></Ionicons>
+        </View>
+      </View>
+      {/* Header */}
+
+
+      {/* Conteudo */}
+      <View style={styles.content}>
+        <Text style={styles.contentTitle}>Bom dia!</Text>
+        <Text style={styles.contentSubtitle}>Que tal um café hoje?</Text>
+
       </View>
 
-      <View>
-        <Ionicons name="person" size={20} color="#923709ff"></Ionicons>
-      </View>
+      {/* Conteudo */}
 
+    </ScrollView>
 
-    </View>
 
   );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor: '#c443ebff'
+    flex: 1,
+    backgroundColor: '#f9f9f9'
   },
 
   header: {
-    width:'100%',
+    width: '100%',
     paddingHorizontal: 24,
-    paddingTop: 100,
+    paddingTop: 60,
     paddingBottom: 20,
-    flexDirection: "column",
+    flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#eee9e9"
+  },
+
+  headerTitle: {
+    fontSize: 22,
+    fontWeight: "800",
+    color: "#2f2d2c"
+  },
+
+  headerSubtitle: {
+    fontSize: 14,
+    color: "#9b9b9b",
+    marginTop: 4,
+  },
+
+  avatarPlaceHolder: {
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    backgroundColor: "#f0f0f0",
+    justifyContent: "center",
     alignItems: "center"
-  }
+  },
+
+  content:{
+    paddingHorizontal: 24,
+  },
+
+  contentTitle: {
+    fontSize: 32,
+    fontWeight: "800",
+    color: "#2f2d2c",
+    marginTop: 8,
+  },
+
+  contentSubtitle: {
+    fontSize: 16,
+    color: "#9b9b9b",
+    marginTop: 8,
+  },
+
+
 })
